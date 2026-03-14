c918756ceb3e9210b57564906b6b62d3620159f3
ydvritesh14 <riteshrajyadav2003@gmail.com>
2026-03-14 19:52:32 +0530

    fix: correct static file path for public folder

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(cookieParser())
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 // app.use(express.static("public"))
-app.use(express.static(path.join(__dirname, "../../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api/v1/videos", videoRoutes);
 
 
